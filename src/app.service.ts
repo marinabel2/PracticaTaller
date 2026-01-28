573013f19ea61d42064cc03926319e63e53b988e
marinabel2 <112512837+marinabel2@users.noreply.github.com>
2026-01-28 09:58:30 -0600

    Update app.service.ts
    Haciendo prueba personal agregando nombre

--- src/app.service.ts
+++ src/app.service.ts
@@ -8,6 +8,7 @@ export class AppService {
         <div style="background-color: rgba(255, 255, 255, 0.1); padding: 40px; border-radius: 15px; box-shadow: 0 4px 15px rgba(0, 0, 0, 0.3); border: 1px solid #00b5e2;">
           <h1 style="color: #00b5e2; font-size: 3.5rem; margin: 0 0 20px 0; text-transform: uppercase; letter-spacing: 2px;">Hola mUNDO docentes Dactic</h1>
           <p style="font-size: 1.8rem; margin: 0; font-weight: 300;">esta es mi primera app web</p>
+           <p style="font-size: 1.8rem; margin: 0; font-weight: 300;">Creada por ek Ing Abel Marin</p>
           <div style="margin-top: 30px; font-size: 1rem; color: #a0c4ff;">
             #SomosOrgullosamenteUNI
           </div>
